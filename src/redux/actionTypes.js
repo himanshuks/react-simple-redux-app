999826e1fc97a505c44b34fb1ba08947e94459d1
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2021-09-27 10:57:29 +0530

    action type for employee added

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,3 +1,8 @@
+// Constants to be used across project
+// Keyword export makes the variable accessible to all components
+
 export const BUG_ADDED = "bugAdded";
 export const BUG_REMOVED = "bugRemoved";
 export const BUG_RESOLVED = "bugResolved";
+
+export const SHOW_EMPLOYEE = "showEmployee";
